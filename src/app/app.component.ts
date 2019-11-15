57f1779f392df3fadccd22649313b15aac080d79
Majd Albandik <majd.albandik@yahoo.com>
2019-11-15 13:49:22 +0100

    startWith operator startWith emits an inital value provided from us and then emits the values from the api (source observable)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { tap, map, pluck, first } from 'rxjs/operators';
+import { tap, map, pluck, first, startWith } from 'rxjs/operators';
 import { Component } from '@angular/core';
-import { range } from 'rxjs';
+import { range, timer } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -11,20 +11,16 @@ export class AppComponent {
     title = 'rxjs-operators';
 
     constructor() {
-        // first takes the first value which meet the condition and complete the stream (no other values will be handelt any more)
-        range(0, 10).pipe(
-            map((index: number) => new Friend(index)),
-            first((friend: Friend) => friend.name === 'name 8')
-        ).subscribe((value) => console.log(value));
-
+        // startWith emits an inital value provided from us and then emits the values from the api (source observable)
+        timer(2000, 1000)
+            .pipe(
+                startWith(10)
+            )
+            .subscribe((value) => console.log(value));
+        // output
+        // immediately 10
+        // after 2000ms the first value comes from the source 0
+        // after 1000ms the seconde value comes from the source 1
     }
 }
 
-class Friend {
-    name: string;
-    matualFriends: string;
-    constructor(public index: number) {
-        this.name = `name ${index}`;
-        this.matualFriends = `matual friends ${index * 5}`;
-    }
-}
