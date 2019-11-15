517ec55ad03a56f4fd5019f927442c6a870659ff
Majd Albandik <majd.albandik@yahoo.com>
2019-11-15 12:38:44 +0100

    timer(duration, [interval]) operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
+import { take } from 'rxjs/operators';
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, timer } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxjs-operators';
-  obs$: Observable<any>;
+    title = 'rxjs-operators';
 
-  constructor() {
-    this.obs$ = of(1);
-  }
+    constructor() {
+        // emits one value after 2000ms like setTimeout
+        timer(2000).subscribe((a) => console.log(a));
+
+        // after 3000 ms it starts to emit one value every 1000ms like interval
+        timer(3000, 1000).subscribe((a) => console.log(a));
+    }
 }
