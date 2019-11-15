e01b058b1598a5543504fad844c664170e043987
Majd Albandik <majd.albandik@yahoo.com>
2019-11-15 11:49:04 +0100

    of operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-operators';
+  obs$: Observable<any>;
+
+  constructor() {
+    this.obs$ = of(1);
+  }
 }
