b8745d4a8df67d6a2096edca23d720533eece1ce
Majd Albandik <majd.albandik@yahoo.com>
2019-11-15 12:45:20 +0100

    interval(time in ms)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { take } from 'rxjs/operators';
 import { Component } from '@angular/core';
-import { Observable, of, timer } from 'rxjs';
+import { Observable, of, timer, interval } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -11,10 +11,7 @@ export class AppComponent {
     title = 'rxjs-operators';
 
     constructor() {
-        // emits one value after 2000ms like setTimeout
-        timer(2000).subscribe((a) => console.log(a));
-
-        // after 3000 ms it starts to emit one value every 1000ms like interval
-        timer(3000, 1000).subscribe((a) => console.log(a));
+        // emits one value every 1000ms
+        interval(1000).subscribe((a) => console.log(a));
     }
 }
