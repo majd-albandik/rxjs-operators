2fbe78ce54cf5b3ea750f8bc06e0e289c448a6ed
Majd Albandik <majd.albandik@yahoo.com>
2019-11-15 13:16:36 +0100

    tap operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { filter } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 import { Component } from '@angular/core';
 import { range } from 'rxjs';
 
@@ -11,10 +11,14 @@ export class AppComponent {
     title = 'rxjs-operators';
 
     constructor() {
-        // filter filters the stream and returns all values which meet the condition in th call back function
-        range(0, 9).pipe(
-            filter((value) => value > 5)
-        ).subscribe((filterdValues) => console.log(filterdValues));
+        // tap used to just do somthing simple like console.log and it cannot change
+        // the value so the values will be passed to the next operator like it is
+        range(1, 2).pipe(
+            map((value) => value * 2),
+            tap((value) => console.log('value after map#1 ', value)),
+            map((value) => value * 2),
+            tap((value) => console.log('value after map#2 ', value))
+        ).subscribe();
 
     }
 }
